25a0ea531d82f7f1453e7e11cec025e70026b727
Daniela Ramírez <55553160+dxniela@users.noreply.github.com>
2022-06-12 20:27:18 -0400

    preguntaReto

--- Parcial I/preguntaReto.py
+++ Parcial I/preguntaReto.py
@@ -1,5 +1,6 @@
 import sympy
+import sys
 from math import *
 f=lambda a,b,n:a if n<1 else b if n<2 else f(b,a+b,n-1)
-n=int(input())
+n=int(sys.argv[1])
 print(f(0,1,floor(log2(sympy.bell(n+1)))+1))
